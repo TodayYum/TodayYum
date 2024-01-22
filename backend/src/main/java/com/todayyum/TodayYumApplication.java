3c57820ecd6c704d07aa4fb778e36af1ef49bf97
DeadBBall <kyg4199@naver.com>
2024-01-22 20:20:57 +0900

    [BE] chore : 프로젝트 생성 및 .gitignore 추가

---
+++ backend/src/main/java/com/todayyum/TodayYumApplication.java
@@ -0,0 +1,13 @@
+package com.todayyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayYumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayYumApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/todayyum/TodayYumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayYumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
