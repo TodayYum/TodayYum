3bcf03281bb9244d04ccf7c31198271d7c859ef4
hehezune <funcodezune@gmail.com>
2024-01-24 18:51:55 +0900

    [FE] env : tailwindCSS 설정파일에 폰트 및 컬러 적용

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      black: '#001F2A',
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      'gray-dark': '#787D85',
+      gray: '#B2B0B0',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#ED5D5D',
+        'primary-container': '#FF8D8D',
+        secondary: '#FFAA46',
+        'secondary-container': '#FFD8AA',
+        error: '#ED3A50',
+        background: '#F8F8F8',
+        correct: '#4966FF',
+        thirdly: '#31E579',
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
-  plugins: [],
-}
-
+};
