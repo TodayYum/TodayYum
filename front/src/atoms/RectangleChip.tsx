760d94ca5b2cf190d68cc1e6e07908fd4c25688e
hehezune <funcodezune@gmail.com>
2024-02-06 11:24:33 +0900

    [FE] feat : 오늘의 얌에 사용될 직사각형꼴 칩 추가

---
+++ front/src/atoms/RectangleChip.tsx
@@ -0,0 +1,17 @@
+import { IRectangleChip } from '../types/components/RectangleChip.types';
+
+/**
+ * RectangleChip : 오늘얌 팻말에 사용되는 직사각형 칩 (round 아주 약간 들어간 것)
+ * @param props
+ * @returns
+ */
+
+function RectangleChip(props: IRectangleChip) {
+  return (
+    <span className="bg-secondary my-auto rounded-small text-base py-[5px] px-2.5 border-secondary  border-[1.5px]">
+      {props.text}
+    </span>
+  );
+}
+
+export default RectangleChip;
