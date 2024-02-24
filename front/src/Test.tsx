f1d48ec415cdfcf9daae37f14e90070110f769ad
hehezune <funcodezune@gmail.com>
2024-02-24 23:36:27 +0900

    [FE] feat : RectanbleButton 테스트

--- front/src/Test.tsx
+++ front/src/Test.tsx
@@ -1,11 +1,19 @@
-import Header from './atoms/Header';
+import { useNavigate } from 'react-router-dom';
+// import Header from './atoms/Header';
 import Navbar from './atoms/Navbar';
+import RectangleButton from './atoms/RectangleButton';
 
 function Test() {
+  const navigate = useNavigate();
   return (
     <div>
-      <Header title="테스트입니다" ateAt="01.01" />
+      {/* <Header title="테스트입니다" ateAt="01.01" /> */}
       <Navbar />
+      <RectangleButton
+        text="이건 테스트"
+        isCancle
+        onClick={() => navigate('login')}
+      />
     </div>
   );
 }
