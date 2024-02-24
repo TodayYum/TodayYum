83d8dcbf272c701a62f1d4d44f4802b8d0330315
hehezune <funcodezune@gmail.com>
2024-02-24 23:36:00 +0900

    [FE] feat : LoginPage 등록에 따른 Route 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Test from './Test';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Test />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
