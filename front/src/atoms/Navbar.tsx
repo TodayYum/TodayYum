3df2b37f29e7be9bcdffed81d20f68c309294290
hehezune <funcodezune@gmail.com>
2024-02-24 10:38:59 +0900

    [FE] feat : Navbar 구현

---
+++ front/src/atoms/Navbar.tsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faMagnifyingGlass,
+  faCirclePlus,
+  faUser,
+  faBell,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className="text-primary w-full text-[35px] flex justify-around fixed bottom-0">
+      <Link to="/">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faCirclePlus} />
+      </Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faUser} />
+      </Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faBell} />
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
