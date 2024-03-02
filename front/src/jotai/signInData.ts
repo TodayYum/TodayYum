df41985a2712055a16774740f0b71d19e42047a6
hehezune <funcodezune@gmail.com>
2024-03-02 15:40:12 +0900

    [FE] refactor : registData 아톰 이름 변경

--- front/src/jotai/signInData.ts
+++ front/src/jotai/signInData.ts
@@ -1,6 +1,6 @@
 /**
  * 회원가입에서 관리하는 정보
- * signUpAtom : initialAtom
+ * signInDataAtom : initialAtom
  * setEmail : 이메일 갱신 set
  * setCode : 인증코드 갱신
  * plusSignUpLevel : signUp 단계 상향
@@ -10,7 +10,7 @@
 import { atom, useAtomValue, useSetAtom } from 'jotai';
 import { ISignUpPage } from '../types/pages/SignUpPage.types';
 
-const signUpAtom = atom<ISignUpPage>({
+const signInDataAtom = atom<ISignUpPage>({
   email: '',
   password: '',
   confirmPassword: '',
@@ -19,78 +19,90 @@ const signUpAtom = atom<ISignUpPage>({
   signUpLevel: 0,
 });
 
+const initSignInDataAtom = atom(null, (get, set) => {
+  set(signInDataAtom, {
+    email: '',
+    password: '',
+    confirmPassword: '',
+    code: '',
+    nickname: '',
+    signUpLevel: 0,
+  });
+});
+
 // setEmail
 const setEmail = atom(null, (get, set, email: string) => {
   const signUpStatus = {
-    ...get(signUpAtom),
+    ...get(signInDataAtom),
     email,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 // setCode
 const setCode = atom(null, (get, set, code: string) => {
   const signUpStatus = {
-    ...get(signUpAtom),
+    ...get(signInDataAtom),
     code,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 // signUpLevel 증가
 const plusSignUpLevel = atom(null, (get, set) => {
-  const prev = { ...get(signUpAtom) };
+  const prev = { ...get(signInDataAtom) };
   const signUpStatus = {
     ...prev,
     signUpLevel: prev.signUpLevel + 1,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 // signUpLevel 감소
 const minusSignUpLevel = atom(null, (get, set) => {
-  const prev = { ...get(signUpAtom) };
+  const prev = { ...get(signInDataAtom) };
   const signUpStatus = {
     ...prev,
     signUpLevel: prev.signUpLevel - 1,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 // password 등록
 const setPassword = atom(null, (get, set, password: string) => {
   const signUpStatus = {
-    ...get(signUpAtom),
+    ...get(signInDataAtom),
     password,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 // 비밀번호 확인
 const setConfirmPassword = atom(null, (get, set, confirmPassword: string) => {
   const signUpStatus = {
-    ...get(signUpAtom),
+    ...get(signInDataAtom),
     confirmPassword,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 // nickname 등록
 const setNickname = atom(null, (get, set, nickname: string) => {
   const signUpStatus = {
-    ...get(signUpAtom),
+    ...get(signInDataAtom),
     nickname,
   };
-  set(signUpAtom, signUpStatus);
+  set(signInDataAtom, signUpStatus);
 });
 
 export const useSetEmailAtom = () => useSetAtom(setEmail);
 export const useSetCodeAtom = () => useSetAtom(setCode);
 export const usePlusSignUpLevelAtom = () => useSetAtom(plusSignUpLevel);
 export const useMinusSignUpLevelAtom = () => useSetAtom(minusSignUpLevel);
-export const useRegistDataAtom = () => useAtomValue(signUpAtom);
+export const useSignInDataAtom = () => useAtomValue(signInDataAtom);
 export const useSetPasswordAtom = () => useSetAtom(setPassword);
 export const useSetConfirmPasswordAtom = () => useSetAtom(setConfirmPassword);
 export const useSetNicknameAtom = () => useSetAtom(setNickname);
+export const useInitSignInDataAtom = () => useSetAtom(initSignInDataAtom);
 
 // 고민 : 여러가지 방법?
 // 1. 지금과 같은 방법
