70d400809cba58e67855f802439392c765c89b93
hehezune <funcodezune@gmail.com>
2024-03-02 15:48:00 +0900

    [FE] feat : 레이아웃 적용하여 회원가입 및 비밀번호 재설정 페이지 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,13 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import Test from './Test';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
+import HeaderOnlyLayout from './layout/HeaderOnlyLayout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Test />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route element={<HeaderOnlyLayout isSignUp />}>
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+      <Route element={<HeaderOnlyLayout isSignUp={false} />}>
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+      </Route>
     </Routes>
   );
 }
