f88945a9714e111080cc7c2a8e05aea3b22f4967
hehezune <funcodezune@gmail.com>
2024-03-03 19:31:42 +0900

    [FE] style : 칩 배경색 목업에 맞게 수정

--- front/src/atoms/DeletableChip.tsx
+++ front/src/atoms/DeletableChip.tsx
@@ -4,6 +4,7 @@ import { IDeletableChip } from '../types/components/DeletableChip.types';
 /**
  *
  * @param props
+ * onClick : 프론트에서 목록 제거 및 백으로 해당 검색어 제거 요청
  * @returns
  */
 
@@ -11,8 +12,8 @@ function DeletableChip(props: IDeletableChip) {
   return (
     <button
       type="button"
-      className="bg-secondary my-auto flex items-center
-      rounded-full text-base py-[5px] px-2.5 border-secondary  border-[1.5px]"
+      className="bg-secondary-container my-auto flex items-center
+      rounded-full text-base py-[5px] px-2.5 border-secondary-container  border-[1.5px]"
     >
       <span className="text-base mr-2">{props.text}</span>
       <FontAwesomeIcon icon={faXmark} onClick={props.onClick} />
