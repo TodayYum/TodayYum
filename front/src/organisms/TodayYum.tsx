15257b6c5f425138e21da36624700b910b6f9966
hehezune <funcodezune@gmail.com>
2024-03-15 23:48:55 +0900

    [FE] feat : 검색 결과 페이지 및 관련 organism 구현

---
+++ front/src/organisms/TodayYum.tsx
@@ -0,0 +1,14 @@
+function TodayYum() {
+  return (
+    <div className="rounded bg-white mx-[30px] py-4 px-2 my-5">
+      <p className="base-bold text-center">오늘의 얌</p>
+      <div>
+        <p className="shadow-md bg-secondary-container base-bold my-auto rounded-small py-[5px] px-2.5 border-secondary-container leading-7  border-[1.5px] w-[40px]">
+          한식
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TodayYum;
