2f0d06bace855959246eedb3407e1e9dfe027c9d
hehezune <funcodezune@gmail.com>
2024-03-31 13:34:32 +0900

    [FE] feat : 정보 창에서 댓글 창으로 넘어갈 수 있게 구현

--- front/src/pages/FilmDetailPage.tsx
+++ front/src/pages/FilmDetailPage.tsx
@@ -4,17 +4,22 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import DetailPageCarousel from '../atoms/DetailPageCarousel';
 import Header from '../atoms/Header';
 import SelectScore from '../atoms/SelectScore';
+import CommentContainer from '../atoms/CommentContainer';
+import CommentList from '../organisms/CommenetList';
+import CommentInput from '../atoms/CommentInput';
 
 function FilmDetailPage() {
   const fixedContentsRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(490);
-
+  const [isMainContents, setIsMainContents] = useState(true);
   const resizeObserver = new ResizeObserver(entries => {
     // 하나만 observe할 것이므로 forEach를 사용하지 않는다.
     if (entries[0].target === fixedContentsRef.current) {
       setHeight(entries[0].contentRect.height + 58);
     }
   });
+  const [commentInputValue, setCommentInputValue] = useState('');
+  console.log(setIsMainContents);
   useEffect(() => {
     if (fixedContentsRef.current) {
       resizeObserver.observe(fixedContentsRef.current);
@@ -24,13 +29,19 @@ function FilmDetailPage() {
       resizeObserver.disconnect();
     };
   }, [fixedContentsRef.current]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCommentInputValue(e.target.value);
+  };
   return (
     <div>
       <Header title="asdf" ateAt="asdf" />
+      {/* 상단부 */}
       <div className="fixed overflow-hidden top-[58px]" ref={fixedContentsRef}>
         <DetailPageCarousel
           imgs={['/t1.jpg', '/t2.jpg', '/t3.jpg', '/t4.jpg']}
         />
+        {/* 프로필 */}
         <div className="bg-white flex justify-between p-[15px]">
           <div className="flex items-center gap-2">
             <img
@@ -46,27 +57,46 @@ function FilmDetailPage() {
           </div>
         </div>
       </div>
-      <div className="bg-white p-[15px]" style={{ marginTop: height }}>
-        <p className="text-base my-[15px]">
-          김ㅁㅁㅁㅁㅁㅁ치말이ㅣㅣㅣㅣ 완전 조ㅗㅗㅗㅗㅗㅗㅗㅗ아
-          ㄹㄹㄹㄹㄹㄹㄹㄹㄹfffffff
-          ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
-          ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
-          ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
-        </p>
-        {DUMMY_TAGLIST.map(element => (
-          <span className="text-base mr-2"># {element}</span>
-        ))}
-        <p className="text-base my-5">서울특별시 강남구 테헤란로 212</p>
-        <SelectScore />
-      </div>
-      <div className="flex justify-between p-3 gap-2 bg-white rounded mx-[15px] fixed bottom-20 w-[calc(100vw-30px)] shadow-lg">
-        <span className="font-bold text-sm flex-[0_0_110px]">닉네임</span>
-        <span className="text-sm flex-[1_1_60%] line-clamp-1">
-          내용용용용용내용용용용용내용용용용용내용용용용용내용용용용용내용용용용용
-        </span>
-      </div>
+      {/* 하단부 */}
+      {isMainContents ? (
+        <div className="bg-white p-[15px]" style={{ marginTop: height }}>
+          <p className="text-base my-[15px]">
+            김ㅁㅁㅁㅁㅁㅁ치말이ㅣㅣㅣㅣ 완전 조ㅗㅗㅗㅗㅗㅗㅗㅗ아
+            ㄹㄹㄹㄹㄹㄹㄹㄹㄹfffffff
+            ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
+            ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
+            ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
+          </p>
+          {DUMMY_TAGLIST.map(element => (
+            <span className="text-base mr-2"># {element}</span>
+          ))}
+          <p className="text-base my-5">서울특별시 강남구 테헤란로 212</p>
+          <SelectScore />
+        </div>
+      ) : (
+        <CommentList
+          onClick={() => setIsMainContents(true)}
+          marginTop={height}
+        />
+      )}
       <div className="bg-black h-36 invisible ">asdf</div>
+      {isMainContents ? (
+        <div
+          onClick={() => setIsMainContents(false)}
+          onKeyUp={() => {}}
+          tabIndex={0}
+          role="button"
+          aria-label="댓글 프리뷰"
+        >
+          <CommentContainer
+            nickname="닉네임"
+            comment="내용용용용용내용용용용용내용용용용용내용용용용용내용용용용용내용용용용용"
+            isPreview
+          />
+        </div>
+      ) : (
+        <CommentInput value={commentInputValue} onChange={handleChange} />
+      )}
     </div>
   );
 }
