ac60780324807035a77015304d97753b9c3380ff
hehezune <funcodezune@gmail.com>
2024-05-05 16:50:31 +0900

    [FE] feat : 댓글 무한스크롤 추가

--- front/src/organisms/CommenetList.tsx
+++ front/src/organisms/CommenetList.tsx
@@ -4,9 +4,24 @@ import { useState } from 'react';
 import CommentContainer from '../atoms/CommentContainer';
 import { ICommentList } from '../types/organisms/CommentList.types';
 import EditComment from '../atoms/EditComment';
+import useIntersect from '../util/useIntersect';
+
+// 연동 시 파일로 뺼 것, edit와 commentcontainer에 extand할 것
+interface IComment {
+  nickname: string;
+  comment: string;
+}
 
 function CommentList(props: ICommentList) {
   const [idxOfEditComment, setIdxOfEditComment] = useState(-1);
+  const [commentList, setCommentList] = useState<IComment[]>(DUMMY_COMMENTLIST);
+  const [, setRef] = useIntersect(async (entry, observer) => {
+    const hasNext = true;
+    const newList = commentList.concat(JSON.parse(JSON.stringify(commentList)));
+    setCommentList(newList);
+    observer.unobserve(entry.target);
+    return hasNext;
+  }, {});
   return (
     <div
       className="bg-white rounded mx-[15px]"
@@ -21,7 +36,7 @@ function CommentList(props: ICommentList) {
         <p className="font-bold text-base">Comments</p>
       </div>
       <div>
-        {DUMMY_COMMENTLIST.map((element, idx) =>
+        {commentList.map((element, idx) =>
           idxOfEditComment === idx ? (
             <EditComment
               commentId={idx}
@@ -36,6 +51,9 @@ function CommentList(props: ICommentList) {
             />
           ),
         )}
+        <div ref={setRef} className="opacity-0">
+          now loading
+        </div>
       </div>
     </div>
   );
