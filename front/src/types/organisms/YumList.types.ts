ab051cfcabc0e5cc9952f6b67e95381e0d7274e3
hehezune <funcodezune@gmail.com>
2024-05-15 12:47:54 +0900

    [FE] feat : 오늘얌 API 연결

--- front/src/types/organisms/YumList.types.ts
+++ front/src/types/organisms/YumList.types.ts
@@ -2,9 +2,9 @@ import { IPolaroidFilm } from './PolaroidFilm.types';
 
 export interface IYumList {
   onClose: () => void;
+  todayYummys: IPolaroidFilm[];
 }
 
 export interface IYumByCategory {
-  title: string;
   board: IPolaroidFilm;
 }
