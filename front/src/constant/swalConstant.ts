78d4fd02e037748e89dd51a96023ccbb046b3542
hehezune <funcodezune@gmail.com>
2024-05-17 22:15:04 +0900

    [FE] feat : Swal 객체를 모아놓는 파일 생성

---
+++ front/src/constant/swalConstant.ts
@@ -0,0 +1,17 @@
+import { SweetAlertOptions } from 'sweetalert2';
+
+export const swalFail: SweetAlertOptions = {
+  icon: 'warning',
+  text: '',
+  width: '300px',
+  confirmButtonText: '돌아가기',
+  confirmButtonColor: '#787D85',
+};
+
+export const swalSuccess: SweetAlertOptions = {
+  icon: 'success',
+  text: '',
+  width: '300px',
+  confirmButtonText: '돌아가기',
+  confirmButtonColor: '#787D85',
+};
