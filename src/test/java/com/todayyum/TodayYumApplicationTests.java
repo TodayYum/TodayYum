588c7710be05fcbbb0518965e75209a9046b7ce3
DeadBBall <kyg4199@naver.com>
2024-07-11 01:30:33 +0900

    [BE] chore : test 파일 수정

--- src/test/java/com/todayyum/TodayYumApplicationTests.java
+++ src/test/java/com/todayyum/TodayYumApplicationTests.java
@@ -3,7 +3,7 @@ package com.todayyum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TodayYumApplicationTests.class)
+//@SpringBootTest(classes = TodayYumApplicationTests.class)
 class TodayYumApplicationTests {
 
     @Test
