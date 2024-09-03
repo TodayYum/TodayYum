238419c1afec2afd6fc96eb22411c1bf6cf6d4e6
DeadBBall <kyg4199@naver.com>
2024-09-03 16:27:54 +0900

    [BE] fix : 캐시 수정

--- src/main/java/com/todayyum/TodayYumApplication.java
+++ src/main/java/com/todayyum/TodayYumApplication.java
@@ -2,12 +2,10 @@ package com.todayyum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableCaching
 public class TodayYumApplication {
 
     public static void main(String[] args) {
