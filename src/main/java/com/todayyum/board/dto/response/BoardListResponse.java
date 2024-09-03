88ad25061ef05c377c5cf7893672a0e8bc2160e1
DeadBBall <kyg4199@naver.com>
2024-09-03 17:57:27 +0900

    [BE] chore : boardListResponse 직렬화

--- src/main/java/com/todayyum/board/dto/response/BoardListResponse.java
+++ src/main/java/com/todayyum/board/dto/response/BoardListResponse.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
-public class BoardListResponse {
+public class BoardListResponse implements Serializable {
 
     private Long id;
     private Long yummyCount;
